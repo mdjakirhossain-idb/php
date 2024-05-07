b058fb2e841bc68438611a0df4120933f57cbec4
mdjakirhossain-idb <jakir310773@gmail.com>
2024-05-07 18:48:06 +0600

    project

---
+++ POS_project5/supplier.php
@@ -0,0 +1,18 @@
+<?php 
+    include("includes/header.php");
+    include("includes/sidebar.php");
+    include("includes/nav.php");
+?>
+
+
+<?php
+include("insert.php");
+?>
+
+
+
+
+?php 
+    include("includes/footer.php");
+   
+?>
